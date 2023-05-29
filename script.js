a8704f823ce94abf6607a50b26f8ce9ab6d8b729
shaxa98 <shakhzodakhrorov@gmail.com>
2023-05-29 21:25:45 +0500

    innerHTML

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ const listContainer = document.getElementById("list-container");
 function addTask() {
   if (inputBox.value === "") {
     alert("You must write somthing!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    listContainer.appendChild(li);
   }
 }
