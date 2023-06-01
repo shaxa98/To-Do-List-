26c05c1b1f8bd85c65fe358d0151c145a78f9280
shaxa98 <shakhzodakhrorov@gmail.com>
2023-06-01 17:50:37 +0500

    finished alll

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function addTask() {
 listContainer.addEventListener(
   "click",
   function (e) {
-    if (e.target.tagName === "Li") {
+    if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
